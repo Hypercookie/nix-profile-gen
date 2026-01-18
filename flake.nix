ce8280dda876f1f8edfbb014cd90f9899f9f3657
jannes <hypercookie@gmx.de>
2026-01-18 11:35:17 +0100

    Flake

--- flake.nix
+++ flake.nix
@@ -4,19 +4,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager }:
-    { system }:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      hm = import home-manager { inherit pkgs; };
-    in
-    {
-      homeModules.myMacModule = hm.lib.homeManagerModule {
-        modules = [
-          ./nix-modules/generateMacOSProfile.nix
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, home-manager, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hm = import home-manager {
+          inherit pkgs;
+          # For Nix-Darwin: don't need 'configuration' here
+          # only importing library functions
+        };
+      in
+      {
+        # Export the module for reuse
+        homeModules.profiles = ./nix-modules/generateMacOSProfile.nix;
+
+        # Optional: export packages or nixosModules if needed
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "dummy"; # just a placeholder if you want
+          buildCommand = "echo 'nothing'";
+        };
+      }
+    );
 }
